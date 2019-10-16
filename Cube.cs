6a2571d102983f9478ce20f69661c6abdd5c78c5
kingofdot <50697575+kingofdot@users.noreply.github.com>
2019-10-16 23:14:23 +0900

    Exp

---
+++ Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Rigidbody m_myrigidbody = null;
+
+    private void OnEnable()
+    {
+        if(m_myrigidbody == null)
+        {
+            m_myrigidbody = GetComponent<Rigidbody>();
+        }
+
+        m_myrigidbody.velocity = Vector3.zero;
+        m_myrigidbody.AddExplosionForce(1000, transform.position, 1f);
+
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(1f);
+        ObjectPoolingManager.instance.insertQueue(gameObject);
+    }
+}
